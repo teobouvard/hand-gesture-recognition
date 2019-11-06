8943ff50763ab39c7a3ce44f8d3cc62b3cc39536
Téo Bouvard <teobouvard@gmail.com>
2019-11-06 13:56:09 +0100

    Better data extraction

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -1,27 +1,50 @@
 import os
-
+from glob import glob
 import cv2
 import numpy as np
 from tqdm import tqdm
 from datetime import datetime
 from image_preprocessing import extract_hand
 
+def clean_data_dir():
+    path = os.path.join('data', 'train', '*', '*.jpg')
+    images = glob(path)
+    for image in images:
+        os.remove(image)  
+
+
 if __name__ == "__main__":
-    video = cv2.VideoCapture('test.mp4')
+    clean_data_dir()
+    videos = glob('videos/*')
 
-    while(True):
-        ret, frame = video.read()
+    for filename in videos:
+        fingers = os.path.basename(filename)[0]
+        video = cv2.VideoCapture(filename)
 
-        hand = extract_hand(frame, crop=True, flatten=False)
-        cv2.imshow('image', hand)
-        #cv2.imwrite(os.path.join('video_snapshots', f'{str(datetime.now())}.jpg'), hand)
+        while(True):
+            ret, frame = video.read()
+            if ret:
+
+                hand = extract_hand(frame, crop=True, size=(50, 50))
+                cv2.imshow(f'Extracting images - {fingers} fingers', hand)
+                cv2.imwrite(os.path.join('data', 'train', fingers, f'{str(datetime.now())}.jpg'), hand)
+
+                # handle keypress
+                keypress = cv2.waitKey(1)
+                if  keypress == ord('q'):
+                    # stop extraction
+                    exit()
+                elif keypress == ord(' '):
+                    # pause video 
+                    cv2.waitKey(0)
+                elif keypress == ord('s'):
+                    # skip this video
+                    break
+
+            else:
+                break
         
-
-        keypress = cv2.waitKey(1)
-        if  keypress == ord('q'):
-            # release the capture
-            video.release()
-            cv2.destroyAllWindows()
-            break
-        elif keypress == ord(' '):
-            cv2.waitKey(0)
+        # release the capture
+        video.release()
+        cv2.destroyAllWindows()
+    
--- src/image_preprocessing.py
+++ src/image_preprocessing.py
@@ -2,17 +2,11 @@ import cv2
 import numpy as np
 import os
 
-MIN_HAND_SIZE = 10000
+MIN_HAND_SIZE = 100000
 
-def extract_hand(img, flatten=True, size=(500, 500), crop=True):
+def extract_hand(img, crop, size=(50, 50)):
     # read image from stream
-    if type(img) == np.ndarray:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    elif os.path.isfile(img):
-        img = cv2.imread(img)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
-    img = crop_square(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # create kernel for morphological operations
     kernel = np.ones((3,3), np.uint8)
@@ -28,25 +22,20 @@ def extract_hand(img, flatten=True, size=(500, 500), crop=True):
     # closing to fill gaps in hand
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=5)
 
-    # contour extraction and filling
-    if crop:
-        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for c in contours:
-            if cv2.contourArea(c) > MIN_HAND_SIZE:
-                img = crop_box(img, c)
+    # contour extraction and bounding box
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c) > MIN_HAND_SIZE:
+            img = bound_box(img, c, crop)
 
     img = cv2.GaussianBlur(img, (3, 3), cv2.BORDER_DEFAULT)
     img = cv2.resize(img, size)
 
-    if flatten:
-        if img.max() != 0:
-            img /= img.max()
-
     return img
 
 
-def crop_box(img, contour, only_draw=False):
-    # get bounding box
+def bound_box(img, contour, crop):
+    # get hand bounding box
     x, y, w, h = cv2.boundingRect(contour)
 
     # center box 
@@ -61,15 +50,8 @@ def crop_box(img, contour, only_draw=False):
         min_y = y + (h - w) // 2
         max_y = y + (h + w) // 2
 
-    if only_draw:
+    if crop:
+        return img[min_y:max_y, min_x:max_x]
+    else:
         cv2.rectangle(img, (min_x, min_y), (max_x, max_y), (255,0,0), 3)
         return img
-    else:
-        return img[min_y:max_y, min_x:max_x]
-
-def crop_square(img):
-    h, w, c = img.shape
-    w_center = w // 2
-    h_center = h // 2
-    min_dim = min(h, w) // 2
-    return img[h_center - min_dim:h_center + min_dim, w_center - min_dim:w_center + min_dim, :]
